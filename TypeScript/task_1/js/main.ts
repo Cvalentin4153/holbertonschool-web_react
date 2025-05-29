ed368f3d50fe93626e4bca63822d57a027498435
cvalentin4153 <cvalentin4153@outlook.com>
2025-05-29 12:28:54 -0400

    Task 3-7

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -42,3 +42,67 @@ interface StudentClass {
     workOnHomework(): string;
     displayName(): string;
 }
+
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome() {
+        return "Working from home";
+    }
+
+    getCoffeeBreak() {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks() {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return "Cannot work from home";
+    }
+    getCoffeeBreak() {
+        return "Cannot have a break";
+    }
+    workTeacherTasks() {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string) {
+    if (typeof salary == "number" && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+function isDirector(employee: DirectorInterface | TeacherInterface) {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects) {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    }
+    return "Teaching History";
+}
